42b52fe0a568047085d904034a6dd22564ce265c
Rex Kerr <ichoran@gmail.com>
2014-08-30 13:35:25 -0700

    Working on tree-building from bytecode via an "Inherit" wrapper class.

--- Usage.scala
+++ Usage.scala
@@ -1,39 +1,54 @@
 package scasm
 
 import scala.util._
+import scala.collection.mutable.{ AnyRefMap => MMap }
 import org.objectweb.asm
 import asm.Opcodes._
 
-case class V[A](var value: A) {}
+trait Named { def name: String }
 
-case class Call(op: Int, owner: String, params: Array[String]) {}
-object NoCall extends Call(-1, "", Array.empty[String]) { override def toString = "Call()" }
+case class V[A <: Named](var value: A) { override def toString = "{" + value.name + "}" }
 
-case class Meth(name: String, params: Array[String], wraps: Call = NoCall) {}
-object NoMeth extends Meth("", Array.empty[String], NoCall) { override def toString = "Meth()" }
+case class Call(op: Int, name: String, params: Array[String], in: V[Meth]) extends Named {
+  def str(from: Meth) = if (from eq in.value) s"Call($op, $name, ${params.mkString(", ")}, ^)" else toString
+  override def toString = s"Call($op, $name, ${params.mkString(", ")}, $in)"
+}
+object NoCall extends Call(-1, "", Array.empty[String], V(NoMeth)) { override def toString = "Call()" }
 
-case class Klas(name: String, sig: String, access: Int, parents: Array[String], methods: Array[Meth], calls: Array[Call]) {}
+case class Meth(name: String, params: Array[String], in: V[Klas]) extends Named {
+  def str(from: Klas) = if (from eq in.value) s"Meth($name, ${params.mkString(", ")}, ^)" else toString
+  override def toString = s"Meth($name, ${params.mkString(", ")}, $in)"
+}
+object NoMeth extends Meth("", Array.empty[String], V(NoKlas)) { override def toString = "Meth()" }
+
+case class Klas(name: String, sig: String, access: Int, parents: Array[String], methods: Array[Meth], calls: Array[Call]) extends Named {
+  def isSingle = name endsWith "$" // TODO--check properly!
+  def isTrait = true // TODO--actually check!
+  def isImpl = true // TODO--actually check!
+  override def toString = s"Klas($name, $sig, $access,, ${parents.mkString(", ")},, ${methods.map(_.str(this)).mkString(", ")},, ${calls.mkString(", ")})"
+}
 object NoKlas extends Klas("", "", -1, Array.empty[String], Array.empty[Meth], Array.empty[Call]) { override def toString = "Klas()" }
 
-class KlasExtractor private () extends asm.ClassVisitor(ASM5) {
-  private[this] var myKlas: Klas = NoKlas
+class KlasExtractor private (listen: Call => Boolean) extends asm.ClassVisitor(ASM5) {
+  private[this] val myKlas: V[Klas] = V(NoKlas)
   private[this] var myMeths: List[V[Meth]] = Nil
   private[this] var myCalls: List[V[Call]] = Nil
   def from(cr: asm.ClassReader): Either[String, Klas] = {
     Try{ cr.accept(this, 0) } match {
-      case Failure(t) => Left(t.toString + ": " + Option(t.getMessage).getOrElse("") + "\n" + t.getStackTrace.map("  "+_).mkString("\n"))
-      case Success(_) => Right(myKlas)
+      case Failure(t) => Left(Usage.explain(t))
+      case Success(_) => Right(myKlas.value)
     }
   }
   
   object MethExtractor extends asm.MethodVisitor(ASM5) {
     override def visitMethodInsn(op: Int, owner: String, name: String, desc: String, itf: Boolean) {
-      myCalls = V(Call(op, owner, Array(name, desc))) :: myCalls
+      val c = Call(op, name, Array(owner, desc), myMeths.head)
+      if (listen(c)) myCalls = V(c) :: myCalls
     }
   }
   
   override def visit(ver: Int, acc: Int, name: String, sig: String, sup: String, iface: Array[String]) {
-    myKlas = myKlas.copy(name = name, sig = sig, access = acc, parents = if (iface.isEmpty) Array(sup) else sup +: iface)
+    myKlas.value = myKlas.value.copy(name = name, sig = sig, access = acc, parents = if (iface.isEmpty) Array(sup) else sup +: iface)
   }
   override def visitSource(src: String, dbg: String) {}
   override def visitOuterClass(owner: String, name: String, desc: String) {}
@@ -42,21 +57,112 @@ class KlasExtractor private () extends asm.ClassVisitor(ASM5) {
   override def visitInnerClass(name: String, outName: String, inName: String, acc: Int) {}
   override def visitField(acc: Int, name: String, desc: String, sig: String, v: Object) = null
   override def visitMethod(acc: Int, name: String, desc: String, sig: String, exc: Array[String]) = {
-    myMeths = V(Meth(name, Array(sig))) :: myMeths
+    myMeths = V(Meth(name, Array(sig, desc), myKlas)) :: myMeths
     MethExtractor
   }
   override def visitEnd {
-    myKlas = myKlas.copy(methods = myMeths.reverseMap(_.value).toArray, calls = myCalls.reverseMap(_.value).toArray)
+    myKlas.value = myKlas.value.copy(methods = myMeths.reverseMap(_.value).toArray, calls = myCalls.reverseMap(_.value).toArray)
   }
 }
 object KlasExtractor {
-  def apply(cr: asm.ClassReader) = (new KlasExtractor).from(cr)
+  def apply(cr: asm.ClassReader, listen: Call => Boolean = _ => true): Either[String, Klas] = (new KlasExtractor(listen)).from(cr)
+  
+  def apply(is: java.io.InputStream): Either[String, Klas] = apply(new asm.ClassReader(is))
+  def apply(b: Array[Byte]): Either[String, Klas] = apply(new asm.ClassReader(b))
+  def apply(s: String): Either[String, Klas] = apply(new asm.ClassReader(s))
 }
+
+class Inherit(val me: Klas, val companion: Option[Klas], val impl: Option[Klas]) extends Named {
+  var touched: Inherit = NoInherit
+  val ancestors = MMap[String, Inherit]()
+  val descendants = MMap[String, Inherit]()
+  def withCompanion(k: Klas) = new Inherit(me, Some(k), impl)
+  def withImpl(k: Klas) = new Inherit(me, companion, Some(k))
+  def isSingle = me.isSingle
+  def isTrait = me.isTrait
+  def isImpl = me.isImpl
+  def name = me.name
+  override def toString = "x" + companion.map(_ => "c").mkString + impl.map(_ => "i").mkString
+}
+object NoInherit extends Inherit(NoKlas, None, None) {}
+
+class Lib(val klases: Array[Klas]) {
+  lazy val inheritance = {
+    val inh = MMap[String, Inherit]()
+    for (k <- klases) { inh += (k.name, new Inherit(k, None, None)) }
+    val names = inh.map{ case (n,_) => n }.toArray
+    for (n <- names if inh contains n) {
+      var k = inh(n)
+      val nc = n + "$"
+      val c = inh.getOrNull(nc)
+      if (!k.isSingle && c != null && c.isSingle) {
+        val kc = k withCompanion c.me
+        inh += (n, kc)
+        inh += (nc, kc)
+        k = kc
+      }
+      if (k.isTrait) {
+        val ni = n + "$class"
+        val i = inh.getOrNull(ni)
+        if (i != null && i.isImpl) {
+          val ki = k withImpl i.me
+          inh += (n, ki)
+          inh += (ni, ki)
+          if (k.companion.isDefined) inh += (nc, ki)
+        }
+      }
+    }
+    val ks = inh.map{ case (_,i) => i.me.name }.toSet.toArray.sorted.map(n => inh(n))
+    for (k <- ks) {
+      for (np <- k.me.parents) {
+        val p = inh.getOrNull(np)
+        if (p != null) {
+          k.ancestors += (np, p)
+          p.descendants += (k.name, k)
+        }
+      }
+    }
+    inh
+  }
+}
+object Lib {
+  def read(f: java.io.File, listen: Call => Boolean = _ => true): Either[Vector[String], Lib] = {
+    if (!f.exists) return Left(Vector("Source library does not exist: " + f.getCanonicalFile.getPath))
+    Try {
+      val zf = new java.util.zip.ZipFile(f)
+      val ks = Array.newBuilder[Klas]
+      val ts = Vector.newBuilder[String]
+      try {
+        val e = zf.entries
+        while (e.hasMoreElements) {
+          val ze = e.nextElement
+          if (ze.getName.endsWith(".class")) {
+            Try{ KlasExtractor(zf.getInputStream(ze)) match {
+              case Left(t) => ts += t
+              case Right(k) => ks += k
+            }} match {
+              case Success(_) =>
+              case Failure(t) => ts += ("Exception reading entry " + ze.getName) + Usage.explain(t)
+            }
+          }
+        }
+      } finally zf.close
+      val problems = ts.result
+      if (problems.length > 0) Left(problems) else Right(new Lib(ks.result))
+    } match {
+      case Success(x) => x
+      case Failure(t) => Left(Vector("Problem reading source library " + f.getCanonicalFile.getPath, Usage.explain(t)))
+    }
+  }
+}
+
 
 class Usage {
 }
 
 object Usage {
-  def apply(b: Array[Byte]) = KlasExtractor(new asm.ClassReader(b))
-  def apply(s: String) = KlasExtractor(new asm.ClassReader(s))
+  def source(s: String) = Lib.read(new java.io.File(s), _ => false)
+  
+  def explain(t: java.lang.Throwable) = 
+    t.toString + ": " + Option(t.getMessage).getOrElse("") + "\n" + t.getStackTrace.map("  "+_).mkString("\n")
 }
